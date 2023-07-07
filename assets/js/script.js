deaacfe6e1b0bc00f13ad96e0751fc054c285aab
Edouard102 <>
2023-07-07 16:26:16 +0000

    add code to check winner, update score

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,6 +73,10 @@ const playGame = (e) => {
 
 // Computer choice
 
+const paper = "paper";
+const scissors = "scissors";
+const rocks = "rocks";
+
 const makeComputerChoice = () => {
     // 0 = Paper
     // 1 = Scissors
@@ -83,14 +87,60 @@ const makeComputerChoice = () => {
     switch (nbAleatoire) {
         case 0:
             imageComputerPaper.classList.add("active");
-            return Paper;
+            return paper;
         case 1:
             imageComputerScissors.classList.add("active");
-            return Scissors;
+            return scissors;
         default:
             imageComputerRocks.classList.add("active");
-            return Rocks;
+            return rocks;
+
     }
 };
+// check winner 
+
+const checkWinner = (playerChoice, computerChoice) => {
+    if (playerChoice == computerChoice) {
+        message.textContent = "Equality !";
+        return;
+    }
+
+    if (playerChoice == rocks) {
+        if (computerChoice == paper) {
+            return victoryComputer();
+        } else if (computerChoice == scissors) {
+            return victoryPlayer();
+        }
+    }
+
+    if (playerChoice == paper) {
+        if (computerChoice == scissors) {
+            return victoryComputer();
+        } else if (computerChoice == rocks) {
+            return victoryPlayer();
+        }
+    }
+
+    if (playerChoice == scissors) {
+        if (computerChoice == rocks) {
+            return victoryComputer();
+        } else if (computerChoice == paper) {
+            return victoryPlayer();
+        }
+    }
+};
+
+//  update score
+
+const victoryComputer = () => {
+    message.textContent = "The computer wins...";
+    scoreComputer.textContent++;
+};
+
+const victoryPlayer = () => {
+    message.textContent = "You win ! :)";
+    scorePlayer.textContent++;
+};
+
 
 btnPlayer.forEach((btn) => btn.addEventListener("click", playGame));
